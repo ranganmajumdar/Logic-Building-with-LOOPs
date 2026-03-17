22df051d157f3d5392eda7136ae2480472007a45
Rangan Majumdar <rangan.majumdar.22@aot.edu.in>
2026-03-17 19:53:44 +0530

    Initial commit

---
+++ p2q1.py
@@ -0,0 +1,3 @@
+# print all numbers from 1 to 10 using for loop
+for i in range(1, 11):
+    print(i)
---
+++ p2q2.py
@@ -0,0 +1,3 @@
+# print numbers from 10 to 1 using for loops
+for i in range(10, 0, -1):
+    print(i)
